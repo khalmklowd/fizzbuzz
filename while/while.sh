060b87651b38265600d2c02ea3087b809a9ac6a3
khalmklowd <kylewmcleod@hotmail.com>
2021-03-02 13:35:39 -0500

    added fizzbuzz function and removed variables

--- while/while.sh
+++ while/while.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 counter=0
-while [ $counter -lt 100 ]; do
+
+function fizzbuzz {
+    while [ $counter -lt 100 ]; do
     let counter+=1
-    let b=counter%3
-    let c=counter%5
-    if [ $c -eq 0 ] && [ $b -eq 0 ]; then
+    if [ $(($counter%5)) -eq 0 ] && [ $(($counter%3)) -eq 0 ]; then
     echo 'fizzbuzz'
-    elif [ $b -eq 0 ]; then
+    elif [ $(($counter%3)) -eq 0 ]; then
     echo 'fizz'
-    elif [ $c -eq 0 ]; then
+    elif [ $(($counter%5)) -eq 0 ]; then
     echo 'buzz'
     else echo $counter;
     fi
-done    
+done    }
+
+fizzbuzz
